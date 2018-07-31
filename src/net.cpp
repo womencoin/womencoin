e87ee06bcc1ff1cad9a5e3dd3b828e0c3bcb8458
zero24x <zero@you-spam.com>
2018-07-31 23:32:49 +0200

    Hardcode seednodes

--- src/net.cpp
+++ src/net.cpp
@@ -1229,7 +1229,8 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"104.200.67.104", "104.200.67.104"},
+    {"women-seed-1.dynu.net", "women-seed-1.dynu.net"},
+	{"women-seed-2.dynu.net", "women-seed-2.dynu.net"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
